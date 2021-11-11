03122d5fe076a9bdb6298e113f878b5461e23df2
SANT1 <sant1ag0777@outlook.com>
2021-11-11 11:56:12 -0500

    removed directories

--- HW6/app/src/main/java/com/example/hw6/Repository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.hw6
-
-class Repository private constructor(){
-    companion object{
-        val noteList= mutableListOf<Note>()
-    }
-}
--- Project4/app/src/main/java/com/example/project4/Song.kt
+++
@@ -1,5 +0,0 @@
-package com.example.project4
-
-import android.net.Uri
-
-data class Song(val image: String, val name: String, val artist: String, val album: String)
