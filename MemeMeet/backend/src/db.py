82f3d4f5270b27a6fdce32062af02d4f9d464815
AJ-Shen <Jialin_Shen@ProtonMail.com>
2021-12-03 22:26:32 -0500

    delete unused comment

--- MemeMeet/backend/src/db.py
+++ MemeMeet/backend/src/db.py
@@ -5,25 +5,17 @@ import os
 import bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
-# from datatime import datetime
 
 db = SQLAlchemy()
 
-# Tags: like Courses
 # Tags: one tag to many posts
-# id name posts users
-
+# id, name, posts, users
 
 class Tags(db.Model):
     __tablename__ = "tags"
     id = db.Column(db.Integer, primary_key=True)
     tag = db.Column(db.String, nullable=False)
     posts = db.relationship("Posts", cascade="delete")
-    # users = db.relationship(
-    #     "Users",
-    #     secondary=association_table_instructors,
-    #     back_populates="courses_to_instructors"
-    # )
 
     def __init__(self, **kwargs):
         self.tag = kwargs.get("tag")
@@ -47,21 +39,15 @@ class Tags(db.Model):
             "tag": self.tag
         }
 
-# Users: like Users
-# Users: one user to many posts
-# id name posts
 
+# Users: one user to many posts
+# id, name, posts
 
 class Users(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     posts = db.relationship("Posts", cascade="delete")
-    # posts_to_user = db.relationship(
-    #     "Posts",
-    #     secondary = association_table_users,
-    #     back_populates="users"
-    # )
 
     def __init__(self, **kwargs):
         self.name = kwargs.get("name")
@@ -81,27 +67,21 @@ class Users(db.Model):
         }
 
 
-# Posts: like Assignments
 # Posts: one post to only one user
 # Posts: one post to only one tag
-# id caption image post_time tag_id user_id
+# id, caption, image, user_id, tag_id
 
 class Posts(db.Model):
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True)
     caption = db.Column(db.String, nullable=False)
     image = db.Column(db.String, nullable=False)
-    # Reference: to implement post_time, we referenced these pages:
-    # https://stackoverflow.com/questions/13370317/sqlalchemy-default-datetime
-    # https://stackoverflow.com/questions/10201648/sqlalchemy-wont-accept-datetime-datetime-now-value-in-a-datetime-column
-    # post_time = db.Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"))
 
     def __init__(self, **kwargs):
         self.caption = kwargs.get("caption")
         self.image = kwargs.get("image")
-        # self.post_time = datetime.datetime.utcnow()
         self.user_id = kwargs.get("user_id")
         self.tag_id = kwargs.get("tag_id")
 
@@ -110,7 +90,6 @@ class Posts(db.Model):
             "id": self.id,
             "caption": self.caption,
             "image": self.image,
-            # "post_time": self.post_time
             "user": to_user.sub_p_serialize(),
             "tag": to_tag.sub_p_serialize()
         }
@@ -120,7 +99,6 @@ class Posts(db.Model):
             "id": self.id,
             "caption": self.caption,
             "image": self.image,
-            # "post_time": self.post_time
             "user": to_user.sub_p_serialize()
         }
 
@@ -129,8 +107,6 @@ class Posts(db.Model):
             "id": self.id,
             "caption": self.caption,
             "image": self.image
-            # "post_time": self.post_time
-            # "tag": to_tag.sub_p_serialize()
         }
 
 
